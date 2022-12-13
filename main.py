3e02469cd66b7632f072bcbac15ff03efa69e8f3
CRS <crsc2222@outlook.com>
2022-12-13 12:47:59 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Equal-Weight S&P 500 Index Fund
+
+#Libary Imports
+import numpy as np
+import pandas as pd
+import requests
+import xlsxwriter
+import math
+
+
+
+#Importing List of Stocks
+#Usually you would connect to index provider/financial data API to get list of companies 
+stocks = pd.read_csv('sp_500_stocks.csv')
+
+#Acquiring an API token
+#Free IEX Cloud API token, free, randomized data
+from secrets import IEX_CLOUD_API_TOKEN
+
